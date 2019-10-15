28796761b3083d5e3d46a467d667d9dd7897d06a
Vyom Chandra Gallani <36289357+vyom153069@users.noreply.github.com>
2019-10-15 11:40:02 +0530

    SolutionOne.java

---
+++ vyom/SolutionOne.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class SolutionOne{
+ public static void main(String[] args)
+ {
+   Scanner sc=new Scanner(System.in);
+   int s=sc.nextInt();
+   for(int i=0;i<s;i++)
+   for(int j=0;j<s;j++)
+   System.out.println("*");
+ }
+}
